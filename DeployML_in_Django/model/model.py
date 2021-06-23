e596b82405b8771a7d63eabb19d1addd82a65e96
swati-gwc <swatitripathi2000@gmail.com>
2021-06-23 14:22:09 +0530

    model added

---
+++ DeployML_in_Django/model/model.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import joblib
+
+dataset=pd.read_csv("glass.csv")
+
+X=dataset.iloc[:,:-1] #independent variables
+y=dataset.iloc[:,9]    #depenedent variables
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+X_train=sc_x.fit_transform(X_train)
+X_test=sc_x.transform(X_test)
+
+
+from sklearn.ensemble import RandomForestClassifier
+cls = RandomForestClassifier(criterion='entropy',n_estimators=300,random_state = 42)
+cls.fit(X_train, y_train)
+
+y_pred = cls.predict(X_test)
+
+
+print('ACCURACY is ',cls.score(X_test,y_test)*100,'%')
+
+filename = 'finalized_model.sav'
+joblib.dump(cls,filename)         
+
+             
+
+             
